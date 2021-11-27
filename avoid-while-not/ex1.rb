4e491267d0822b6b9009aec6edd30b6aaf627b33
kmsheng <kmsh3ng@gmail.com>
2021-11-27 13:16:10 +0800

    add example of avoiding while not

---
+++ avoid-while-not/ex1.rb
@@ -0,0 +1,20 @@
+max_tries = 3
+tries = 0
+
+# "while not" could be replaced by "until"
+puts "While `tries` are not greater than `max_tries`..."
+while ! (tries >= max_tries)
+  tries += 1
+  puts "tries: #{tries}"
+end
+puts "Done."
+
+# A better approach
+tries = 0
+puts "\n\n"
+puts "Use the keyword `until`...."
+until tries >= max_tries
+  tries += 1
+  puts "tries: #{tries}"
+end
+puts "Done."
