7b106b58500c1a2d2de2dfcf57fe9aae25a61986
kmsheng <kmsh3ng@gmail.com>
2021-11-27 14:01:40 +0800

    fix rubocop errors

--- avoid-while-not/ex1.rb
+++ avoid-while-not/ex1.rb
@@ -4,8 +4,9 @@ max_tries = 3
 tries = 0
 
 # "while not" could be replaced by "until"
-puts "While `tries` are not greater than `max_tries`..."
-while ! (tries >= max_tries)
+puts "While 'tries' are not greater than 'max_tries'..."
+
+while ! (tries >= max_tries) # rubocop:disable all
   tries += 1
   puts "tries: #{tries}"
 end
