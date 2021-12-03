dcc9509a109dc64ec0577373900858628324fbaf
kmsheng <kmsh3ng@gmail.com>
2021-12-03 16:28:33 +0800

    add return.rb

---
+++ return/return.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+def stop_me
+  return 'good' # rubocop:disable all
+end
+
+p stop_me
+
+p 'ok'
+
+arr = [1, 2, 3].select do |_num|
+  next true
+end
+
+p arr
+
+# Ruby's return in block differs from JavaScript...
+# It stops the program...
+arr = [1, 2, 3].select do |_num|
+  return true # rubocop:disable all
+end
+
+p arr
+
+p 'This is the end of line'
