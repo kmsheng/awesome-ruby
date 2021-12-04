2f33d3c80601d7415d13d342202d330920550b4d
kmsheng <kmsh3ng@gmail.com>
2021-12-04 13:38:14 +0800

    add symbol.rb

---
+++ symbol/symbol.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# same object id comparision
+puts :apple.object_id == :apple.object_id # rubocop:disable all
+
+# string object id comparision
+puts 'apple'.object_id == 'apple'.object_id #rubocop:disable all
