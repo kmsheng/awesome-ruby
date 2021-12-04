26c878f1e1c9c4717137166cb7346c2a8728874d
kmsheng <kmsh3ng@gmail.com>
2021-12-04 13:32:15 +0800

    add double_bang.rb

---
+++ double_bang/double_bang.rb
@@ -0,0 +1,24 @@
+class Person
+
+  def initialize(first_name, last_name, middle_name = nil)
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
+  end
+
+  def has_middle_name?
+    !! @middle_name
+  end
+
+  def set_middle_name(middle_name)
+    @middle_name = middle_name
+  end
+end
+
+p = Person.new('Kuan', 'Sheng')
+puts "Has middle name ? #{p.has_middle_name?}"
+
+p.set_middle_name 'Min'
+
+puts 'Check Again.'
+puts "Has middle name ? #{p.has_middle_name?}"
