d026446e1415a6e755d52bb2361ebc7a1b66427e
kmsheng <kmsh3ng@gmail.com>
2021-12-04 12:41:45 +0800

    add one.rb

---
+++ enumerable/one.rb
@@ -0,0 +1,10 @@
+arr = [1]
+res = arr.one?
+
+puts "Array with only one element: #{res}"
+
+arr2 = [1, 2, 3]
+
+# at least one
+res = arr2.one?{ |num| num == 3 }
+puts "At least one element is equaled to #{res}"
