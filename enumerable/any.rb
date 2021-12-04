5f7d6f08a97e64e5a5e7c8b62a6c6c1f09ef5e8a
kmsheng <kmsh3ng@gmail.com>
2021-12-04 12:24:40 +0800

    update any.rb

--- enumerable/any.rb
+++ enumerable/any.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 arr = %i[test ok wtf]
 
 # Check if any symbols is started with the letter t
-p arr.any?{ |word| word.to_s.start_with? 't' }
+res = arr.any? { |word| word.to_s.start_with? 't' }
+
+puts "Any symbols start with the letter t ?: #{res}"
