99f29e54ed6a2112b70be4279a0b7b15a165971b
kmsheng <kmsh3ng@gmail.com>
2021-12-04 13:10:13 +0800

    add case_when.rb

---
+++ case_when/case_when.rb
@@ -0,0 +1,14 @@
+puts 'What is your favorite programming language ?'
+
+lang = gets.chomp
+
+case lang
+when 'Ruby'
+  puts 'Good to know. Have you learned Rails ?'
+when 'PHP'
+  puts 'Good. Have you used Laraval ?'
+when 'Golang'
+  puts 'Strong type language.'
+else
+  puts 'Never heard of it.'
+end
